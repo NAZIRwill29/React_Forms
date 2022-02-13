d7fbd993b951c4fa3ef26425376532cfaf4d22a0
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 20:35:46 +0800

    usage of func to chg in form add onSubmit={ func } in form and type="submit in button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,13 +21,15 @@ function App() {
   return (
     <div className="container">
       <h1>{headingText}</h1>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="What's your name?"
-        value={name}
-      />
-      <button onClick={handleClick}>Submit</button>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
