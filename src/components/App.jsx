61f5a0154e735e6894b5b35ccb3d8b68d3bb7c9b
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 20:26:15 +0800

    only when click btn , the h1 will chg based on input double the useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
 function App() {
+  //for get value for input
+  const [nameInput, setNameInput] = useState("");
+  // for set value h1
   const [name, setName] = useState("Hello");
   // handle change of input
   function handleChange(event) {
     // event.target.value = get the value in input
     // console.log(event.target.value);
-    setName(event.target.value);
+    setNameInput(event.target.value);
+  }
+
+  //handle click btn
+  function handleClick() {
+    //set value h1
+    setName(nameInput);
   }
 
   return (
@@ -16,9 +25,9 @@ function App() {
         onChange={handleChange}
         type="text"
         placeholder="What's your name?"
-        value={name}
+        value={nameInput}
       />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
