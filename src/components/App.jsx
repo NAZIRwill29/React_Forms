6849fa7b12f454fbb012f3ca440a368973338309
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 20:13:20 +0800

    make useState to chg the h1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
   // handle change of input
   function handleChange(event) {
     // event.target.value = get the value in input
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    setName(event.target.value);
   }
 
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{name}</h1>
       <input
         onChange={handleChange}
         type="text"
