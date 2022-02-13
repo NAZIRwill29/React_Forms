cc0fe4d63216f08d594eb0b15769be1ab0b323ce
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 20:10:29 +0800

    onchange in input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 function App() {
+  // handle change of input
+  function handleChange(event) {
+    // event.target.value = get the value in input
+    console.log(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
       <button>Submit</button>
     </div>
   );
