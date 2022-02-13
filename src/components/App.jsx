715cd909ba14c83d28da46adbd257d81c505093b
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 20:30:16 +0800

    make it more readable for previous

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,30 @@ import React, { useState } from "react";
 
 function App() {
   //for get value for input
-  const [nameInput, setNameInput] = useState("");
+  const [name, setName] = useState("");
   // for set value h1
-  const [name, setName] = useState("Hello");
+  const [headingText, setHeadingText] = useState("Hello");
   // handle change of input
   function handleChange(event) {
     // event.target.value = get the value in input
     // console.log(event.target.value);
-    setNameInput(event.target.value);
+    setName(event.target.value);
   }
 
   //handle click btn
   function handleClick() {
     //set value h1
-    setName(nameInput);
+    setHeadingText(name);
   }
 
   return (
     <div className="container">
-      <h1>{name}</h1>
+      <h1>{headingText}</h1>
       <input
         onChange={handleChange}
         type="text"
         placeholder="What's your name?"
-        value={nameInput}
+        value={name}
       />
       <button onClick={handleClick}>Submit</button>
     </div>
